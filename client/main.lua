2cb785712ea81c2ffed5ed9497f0fbe633d6807f
Testarossa <austin.w.burrows@gmail.com>
2022-10-11 18:33:40 -0400

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,8 +1,7 @@
-local QBCore = exports['qb-core']:GetCoreObject()
 local gameBuild = GetGameBuildNumber()
 
-RegisterNetEvent('qb-chameleonpaint:client:sprayVehicle')
-AddEventHandler('qb-chameleonpaint:client:sprayVehicle', function(name, index)
+RegisterNetEvent('chameleonpaint:client:sprayVehicle')
+AddEventHandler('chameleonpaint:client:sprayVehicle', function(name, color)
     local ped = PlayerPedId()
     if not IsPedInAnyVehicle(ped, false) then
         local dict1, anim1 = 'switch@franklin@lamar_tagging_wall', 'lamar_tagging_wall_loop_lamar'
@@ -27,29 +26,50 @@ AddEventHandler('qb-chameleonpaint:client:sprayVehicle', function(name, index)
         }, {}, {}, function()
             ClearPedTasks(ped)
             TriggerServerEvent("InteractSound_SV:PlayOnSource", "spraypaint", 0.3)
-            QBCore.Functions.Progressbar("painting", 'Painting', 15000, false, true, {
-                disableMovement = true,
-                disableCarMovement = true,
-                disableMouse = false,
-                disableCombat = true,
-            }, {
-                animDict = dict1,
-                anim = anim2,
-                flags = 1,
-            }, {}, {}, function()
-                local pedCoords = GetEntityCoords(ped)
-                local vehicle = QBCore.Functions.GetClosestVehicle(pedCoords)
-                SetVehicleModKit(vehicle, 0)
-                SetVehicleColours(vehicle, Config.ChameleonColors[index][gameBuild], Config.ChameleonColors[index][gameBuild])
-                DeleteObject(prop)
+            if lib.progressBar({
+            duration = 1000,
+            label = 'Shaking can',
+            useWhileDead = false,
+            canCancel = true,
+            disable = {
+                car = true, 
+            },
+            anim = {
+                dict = dict1,
+                clip = anim1,
+                flag = 1 
+            },
+        }) 
+            then
                 ClearPedTasks(ped)
-            end, function() -- Cancel
-                DeleteObject(prop)
-                ClearPedTasks(ped)
-            end)
-        end, function() -- Cancel
+                if lib.progressBar({
+                    duration = 1000,
+                    label = 'Painting',
+                    useWhileDead = false,
+                    canCancel = true,
+                    disable = {
+                        car = true, 
+                    },
+                    anim = {
+                        dict = dict1,
+                        clip = anim2,
+                        flag = 1 
+                    },
+                }) 
+                then
+                    local pedCoords = GetEntityCoords(ped)
+                    local vehicle = lib.getClosestVehicle(pedCoords, 3.0)
+                    SetVehicleModKit(vehicle, 0)
+                    SetVehicleColours(vehicle, color, color)
+                    DeleteObject(prop)
+                    ClearPedTasks(ped) 
+                else 
+                    DeleteObject(prop)
+                    ClearPedTasks(ped)
+                end 
+        else
             DeleteObject(prop)
             ClearPedTasks(ped)
-        end)
+        end
     end
 end)
